5935878ef6d522888f49705617465fc50cadbe75
Kyle <kyle@kyles-imac.gateway.2wire.net>
2016-02-06 00:34:29 -0500

    memory tests with big array

---
+++ app/src/main/java/kyleilantzis/bigarray/BigArrayApplication.java
@@ -0,0 +1,31 @@
+package kyleilantzis.bigarray;
+
+import android.app.Application;
+import android.util.Log;
+
+/**
+ * Created by kyle on 16-02-05.
+ */
+public class BigArrayApplication extends Application {
+
+//    private static final String TAG = BigArrayApplication.class.getSimpleName();
+//
+//    private BigArrayHolder mBigArrayHolder;
+//
+//    public void onCreate() {
+//        super.onCreate();
+//        Log.d(TAG, "onCreate");
+//
+//        mBigArrayHolder = createBigArrayHolder();
+//    }
+//
+//    private BigArrayHolder createBigArrayHolder() {
+//        BigArrayHolder bigArrayHolder = new BigArrayHolder();
+//        bigArrayHolder.randomize();
+//        return bigArrayHolder;
+//    }
+//
+//    public BigArrayHolder getBigArrayHolder() {
+//        return mBigArrayHolder;
+//    }
+}
---
+++ app/src/main/java/kyleilantzis/bigarray/MainActivity.java
@@ -0,0 +1,105 @@
+package kyleilantzis.bigarray;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private BigArrayHolder mBigArrayHolder;
+
+    private TextView mTextView;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+
+        BigArrayHolder bigArrayHolder = (BigArrayHolder) getLastCustomNonConfigurationInstance();
+
+        mBigArrayHolder = bigArrayHolder != null ? bigArrayHolder : BigArrayHolder.newInstance();
+
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text_view);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+
+        String text = Integer.toString(mBigArrayHolder.getRandom());
+
+        mTextView.setText(text);
+    }
+
+    @Override
+    public Object onRetainCustomNonConfigurationInstance() {
+        return mBigArrayHolder;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+        // mBigArrayHolder = null;
+        // System.gc();
+    }
+
+    public static class BigArrayHolder {
+
+        private final int[] mBigArray;
+
+        private final Random mRandom;
+
+        public static BigArrayHolder newInstance() {
+            BigArrayHolder bigArrayHolder = new BigArrayHolder();
+            bigArrayHolder.randomize();
+            return bigArrayHolder;
+        }
+
+        private BigArrayHolder() {
+            mBigArray = new int[5000000];
+            mRandom = new Random();
+        }
+
+        public synchronized void randomize() {
+            for (int i = 0, len = mBigArray.length; i < len; i++) {
+                mBigArray[i] = mRandom.nextInt();
+            }
+        }
+
+        public synchronized int getRandom() {
+            return mBigArray[mRandom.nextInt(mBigArray.length)];
+        }
+    }
+}
